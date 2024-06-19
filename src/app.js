65caa1e980b09be9e72f8b3cad0e767814063331
aryanlandge-01 <landgearyan0@gmail.com>
2024-06-19 12:03:34 +0530

    All Setup Configure

--- src/app.js
+++ src/app.js
@@ -16,4 +16,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes imports like this
+
+import userRouter from "./routes/user.route.js";
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+
+
 export { app }
